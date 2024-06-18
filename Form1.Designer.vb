ba527e8f30b5100cecec58eff8738faf0817df9d
Purva Waykole <113341495+purvacode-80@users.noreply.github.com>
2024-06-18 17:25:26 +0530

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,368 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button0 = New System.Windows.Forms.Button()
+        Me.Sign = New System.Windows.Forms.Button()
+        Me.Dot = New System.Windows.Forms.Button()
+        Me.Plus = New System.Windows.Forms.Button()
+        Me.Minus = New System.Windows.Forms.Button()
+        Me.Multiply = New System.Windows.Forms.Button()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.Clear = New System.Windows.Forms.Button()
+        Me.Backspace = New System.Windows.Forms.Button()
+        Me.Square = New System.Windows.Forms.Button()
+        Me.Result = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Divide = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.PeachPuff
+        Me.Button1.Font = New System.Drawing.Font("Cambria", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(106, 331)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(6)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(85, 50)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "1"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.PeachPuff
+        Me.Button2.Font = New System.Drawing.Font("Cambria", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(217, 331)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(6)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(85, 50)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "2"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.PeachPuff
+        Me.Button3.Font = New System.Drawing.Font("Cambria", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(325, 331)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(6)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(85, 50)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "3"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.PeachPuff
+        Me.Button4.Font = New System.Drawing.Font("Cambria", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(106, 256)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(6)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(85, 50)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "4"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.PeachPuff
+        Me.Button5.Font = New System.Drawing.Font("Cambria", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Location = New System.Drawing.Point(217, 256)
+        Me.Button5.Margin = New System.Windows.Forms.Padding(6)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(85, 50)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "5"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.Color.PeachPuff
+        Me.Button6.Font = New System.Drawing.Font("Cambria", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.Location = New System.Drawing.Point(325, 256)
+        Me.Button6.Margin = New System.Windows.Forms.Padding(6)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(85, 50)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = "6"
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Button7
+        '
+        Me.Button7.BackColor = System.Drawing.Color.PeachPuff
+        Me.Button7.Font = New System.Drawing.Font("Cambria", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button7.Location = New System.Drawing.Point(106, 183)
+        Me.Button7.Margin = New System.Windows.Forms.Padding(6)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(85, 50)
+        Me.Button7.TabIndex = 6
+        Me.Button7.Text = "7"
+        Me.Button7.UseVisualStyleBackColor = False
+        '
+        'Button8
+        '
+        Me.Button8.BackColor = System.Drawing.Color.PeachPuff
+        Me.Button8.Font = New System.Drawing.Font("Cambria", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button8.Location = New System.Drawing.Point(217, 183)
+        Me.Button8.Margin = New System.Windows.Forms.Padding(6)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(85, 50)
+        Me.Button8.TabIndex = 7
+        Me.Button8.Text = "8"
+        Me.Button8.UseVisualStyleBackColor = False
+        '
+        'Button9
+        '
+        Me.Button9.BackColor = System.Drawing.Color.PeachPuff
+        Me.Button9.Font = New System.Drawing.Font("Cambria", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button9.Location = New System.Drawing.Point(325, 183)
+        Me.Button9.Margin = New System.Windows.Forms.Padding(6)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(85, 50)
+        Me.Button9.TabIndex = 8
+        Me.Button9.Text = "9"
+        Me.Button9.UseVisualStyleBackColor = False
+        '
+        'Button0
+        '
+        Me.Button0.BackColor = System.Drawing.Color.PeachPuff
+        Me.Button0.Location = New System.Drawing.Point(106, 408)
+        Me.Button0.Margin = New System.Windows.Forms.Padding(6)
+        Me.Button0.Name = "Button0"
+        Me.Button0.Size = New System.Drawing.Size(85, 50)
+        Me.Button0.TabIndex = 9
+        Me.Button0.Text = "0"
+        Me.Button0.UseVisualStyleBackColor = False
+        '
+        'Sign
+        '
+        Me.Sign.BackColor = System.Drawing.Color.PeachPuff
+        Me.Sign.Location = New System.Drawing.Point(217, 408)
+        Me.Sign.Margin = New System.Windows.Forms.Padding(6)
+        Me.Sign.Name = "Sign"
+        Me.Sign.Size = New System.Drawing.Size(85, 50)
+        Me.Sign.TabIndex = 10
+        Me.Sign.Text = "-/+"
+        Me.Sign.UseVisualStyleBackColor = False
+        '
+        'Dot
+        '
+        Me.Dot.BackColor = System.Drawing.Color.PeachPuff
+        Me.Dot.Location = New System.Drawing.Point(325, 408)
+        Me.Dot.Margin = New System.Windows.Forms.Padding(6)
+        Me.Dot.Name = "Dot"
+        Me.Dot.Size = New System.Drawing.Size(85, 50)
+        Me.Dot.TabIndex = 11
+        Me.Dot.Text = "."
+        Me.Dot.UseVisualStyleBackColor = False
+        '
+        'Plus
+        '
+        Me.Plus.BackColor = System.Drawing.Color.Crimson
+        Me.Plus.ForeColor = System.Drawing.Color.PeachPuff
+        Me.Plus.Location = New System.Drawing.Point(436, 183)
+        Me.Plus.Margin = New System.Windows.Forms.Padding(6)
+        Me.Plus.Name = "Plus"
+        Me.Plus.Size = New System.Drawing.Size(85, 50)
+        Me.Plus.TabIndex = 12
+        Me.Plus.Text = "+"
+        Me.Plus.UseVisualStyleBackColor = False
+        '
+        'Minus
+        '
+        Me.Minus.BackColor = System.Drawing.Color.Crimson
+        Me.Minus.ForeColor = System.Drawing.Color.PeachPuff
+        Me.Minus.Location = New System.Drawing.Point(436, 256)
+        Me.Minus.Margin = New System.Windows.Forms.Padding(6)
+        Me.Minus.Name = "Minus"
+        Me.Minus.Size = New System.Drawing.Size(85, 50)
+        Me.Minus.TabIndex = 13
+        Me.Minus.Text = "-"
+        Me.Minus.UseVisualStyleBackColor = False
+        '
+        'Multiply
+        '
+        Me.Multiply.BackColor = System.Drawing.Color.Crimson
+        Me.Multiply.ForeColor = System.Drawing.Color.PeachPuff
+        Me.Multiply.Location = New System.Drawing.Point(436, 331)
+        Me.Multiply.Margin = New System.Windows.Forms.Padding(6)
+        Me.Multiply.Name = "Multiply"
+        Me.Multiply.Size = New System.Drawing.Size(85, 50)
+        Me.Multiply.TabIndex = 14
+        Me.Multiply.Text = "*"
+        Me.Multiply.UseVisualStyleBackColor = False
+        '
+        'Button16
+        '
+        Me.Button16.Location = New System.Drawing.Point(900, 702)
+        Me.Button16.Margin = New System.Windows.Forms.Padding(6)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(170, 96)
+        Me.Button16.TabIndex = 15
+        Me.Button16.Text = "Button16"
+        Me.Button16.UseVisualStyleBackColor = True
+        '
+        'Clear
+        '
+        Me.Clear.BackColor = System.Drawing.Color.PeachPuff
+        Me.Clear.Location = New System.Drawing.Point(106, 111)
+        Me.Clear.Margin = New System.Windows.Forms.Padding(6)
+        Me.Clear.Name = "Clear"
+        Me.Clear.Size = New System.Drawing.Size(85, 50)
+        Me.Clear.TabIndex = 16
+        Me.Clear.Text = "AC"
+        Me.Clear.UseVisualStyleBackColor = False
+        '
+        'Backspace
+        '
+        Me.Backspace.BackColor = System.Drawing.Color.PeachPuff
+        Me.Backspace.Location = New System.Drawing.Point(217, 111)
+        Me.Backspace.Margin = New System.Windows.Forms.Padding(6)
+        Me.Backspace.Name = "Backspace"
+        Me.Backspace.Size = New System.Drawing.Size(85, 50)
+        Me.Backspace.TabIndex = 17
+        Me.Backspace.Text = "<--"
+        Me.Backspace.UseVisualStyleBackColor = False
+        '
+        'Square
+        '
+        Me.Square.BackColor = System.Drawing.Color.PeachPuff
+        Me.Square.Location = New System.Drawing.Point(325, 111)
+        Me.Square.Margin = New System.Windows.Forms.Padding(6)
+        Me.Square.Name = "Square"
+        Me.Square.Size = New System.Drawing.Size(85, 50)
+        Me.Square.TabIndex = 18
+        Me.Square.Text = "^"
+        Me.Square.UseVisualStyleBackColor = False
+        '
+        'Result
+        '
+        Me.Result.BackColor = System.Drawing.Color.Crimson
+        Me.Result.ForeColor = System.Drawing.Color.PeachPuff
+        Me.Result.Location = New System.Drawing.Point(436, 111)
+        Me.Result.Margin = New System.Windows.Forms.Padding(6)
+        Me.Result.Name = "Result"
+        Me.Result.Size = New System.Drawing.Size(85, 50)
+        Me.Result.TabIndex = 19
+        Me.Result.Text = "="
+        Me.Result.UseVisualStyleBackColor = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.PeachPuff
+        Me.TextBox1.ForeColor = System.Drawing.Color.Black
+        Me.TextBox1.Location = New System.Drawing.Point(106, 49)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(6)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(426, 32)
+        Me.TextBox1.TabIndex = 20
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Divide
+        '
+        Me.Divide.BackColor = System.Drawing.Color.Crimson
+        Me.Divide.ForeColor = System.Drawing.Color.PeachPuff
+        Me.Divide.Location = New System.Drawing.Point(436, 408)
+        Me.Divide.Name = "Divide"
+        Me.Divide.Size = New System.Drawing.Size(85, 50)
+        Me.Divide.TabIndex = 21
+        Me.Divide.Text = "/"
+        Me.Divide.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(644, 518)
+        Me.Controls.Add(Me.Divide)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Result)
+        Me.Controls.Add(Me.Square)
+        Me.Controls.Add(Me.Backspace)
+        Me.Controls.Add(Me.Clear)
+        Me.Controls.Add(Me.Button16)
+        Me.Controls.Add(Me.Multiply)
+        Me.Controls.Add(Me.Minus)
+        Me.Controls.Add(Me.Plus)
+        Me.Controls.Add(Me.Dot)
+        Me.Controls.Add(Me.Sign)
+        Me.Controls.Add(Me.Button0)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.DoubleBuffered = True
+        Me.Font = New System.Drawing.Font("Cambria", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Margin = New System.Windows.Forms.Padding(6)
+        Me.Name = "Form1"
+        Me.Text = "Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button0 As Button
+    Friend WithEvents Sign As Button
+    Friend WithEvents Dot As Button
+    Friend WithEvents Plus As Button
+    Friend WithEvents Minus As Button
+    Friend WithEvents Multiply As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Clear As Button
+    Friend WithEvents Backspace As Button
+    Friend WithEvents Square As Button
+    Friend WithEvents Result As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Divide As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,124 @@
+﻿Public Class Form1
+    Dim Operand1 As Double
+    Dim Operand2 As Double
+    Dim _Operator As String
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox1.Text &= 1
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Text &= "2"
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        TextBox1.Text &= "3"
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        TextBox1.Text &= "4"
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        TextBox1.Text &= "5"
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        TextBox1.Text &= "6"
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        TextBox1.Text &= "7"
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        TextBox1.Text &= "8"
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        TextBox1.Text &= "9"
+    End Sub
+
+    Private Sub Button0_Click(sender As Object, e As EventArgs) Handles Button0.Click
+        TextBox1.Text &= "0"
+    End Sub
+
+    Private Sub Clear_Click(sender As Object, e As EventArgs) Handles Clear.Click
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub Plus_Click(sender As Object, e As EventArgs) Handles Plus.Click
+        Operand1 = Val(TextBox1.Text)
+        TextBox1.Text = ""
+        TextBox1.Focus()
+        _Operator = "+"
+    End Sub
+
+    Private Sub Minus_Click(sender As Object, e As EventArgs) Handles Minus.Click
+        Operand1 = Val(TextBox1.Text)
+        TextBox1.Text = ""
+        TextBox1.Focus()
+        _Operator = "-"
+    End Sub
+
+    Private Sub Multiply_Click(sender As Object, e As EventArgs) Handles Multiply.Click
+        Operand1 = Val(TextBox1.Text)
+        TextBox1.Text = ""
+        TextBox1.Focus()
+        _Operator = "*"
+    End Sub
+
+    Private Sub Divide_Click(sender As Object, e As EventArgs) Handles Divide.Click
+        Operand1 = Val(TextBox1.Text)
+        TextBox1.Text = ""
+        TextBox1.Focus()
+        _Operator = "/"
+    End Sub
+
+    Private Sub Square_Click(sender As Object, e As EventArgs) Handles Square.Click
+        Operand1 = Val(TextBox1.Text)
+        TextBox1.Text = ""
+        TextBox1.Focus()
+        _Operator = "^"
+    End Sub
+
+    Private Sub Dot_Click(sender As Object, e As EventArgs) Handles Dot.Click
+        If InStr(TextBox1.Text, ".") > 0 Then
+            Exit Sub
+        Else
+            TextBox1.Text = TextBox1.Text & "."
+        End If
+    End Sub
+
+    Private Sub Backspace_Click(sender As Object, e As EventArgs) Handles Backspace.Click
+        Dim originalString As String = TextBox1.Text
+        Dim newString As String
+        newString = originalString.Substring(0, originalString.Length - 1)
+        TextBox1.Text = newString
+    End Sub
+
+    Private Sub Result_Click(sender As Object, e As EventArgs) Handles Result.Click
+        Dim Result As Double
+        Operand2 = Val(TextBox1.Text)
+
+        Select Case _Operator
+            Case "+"
+                Result = Operand1 + Operand2
+                TextBox1.Text = Result.ToString()
+            Case "-"
+                Result = Operand1 - Operand2
+                TextBox1.Text = Result.ToString()
+            Case "/"
+                Result = Operand1 / Operand2
+                TextBox1.Text = Result.ToString()
+            Case "*"
+                Result = Operand1 * Operand2
+                TextBox1.Text = Result.ToString()
+            Case "^"
+                Result = Operand1 ^ Operand2
+                TextBox1.Text = Result.ToString()
+        End Select
+        TextBox1.Text = Result.ToString()
+    End Sub
+
+End Class
